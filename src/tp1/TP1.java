5608d3876e990a9010ef424760a0bc695e13572d
Paul <paul221122112211@gmail.com>
2021-10-04 12:00:38 +0200

    enregistrement

--- src/tp1/TP1.java
+++ src/tp1/TP1.java
@@ -4,6 +4,8 @@
  */
 package tp1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author danielthomas
@@ -14,7 +16,14 @@ public class TP1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+       
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("entrer un premier entier");
+        System.out.println("entrer un deuxième entier");
+        int ent1 = sc.nextInt();
+        int ent2 = sc.nextInt();
         // TODO code application logic here
     }
-    
+
 }
