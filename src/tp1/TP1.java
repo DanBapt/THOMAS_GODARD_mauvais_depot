423ec7247d52ea4b9bfc5a62dfb4b403eaf28255
Paul <paul221122112211@gmail.com>
2021-10-04 12:11:41 +0200

    enregistrement

--- src/tp1/TP1.java
+++ src/tp1/TP1.java
@@ -16,14 +16,22 @@ public class TP1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+
         Scanner sc;
         sc = new Scanner(System.in);
         System.out.println("entrer un premier entier");
         System.out.println("entrer un deuxième entier");
         int ent1 = sc.nextInt();
         int ent2 = sc.nextInt();
-        // TODO code application logic here
+        int add = ent1 + ent2;
+        int minus = ent1 - ent2;
+        int mult = ent1 * ent2;
+        float div = ent1 / ent2;
+        System.out.println("La somme des entiers est: " + add);
+        System.out.println("La différence des entiers est: " + minus);
+        System.out.println("La multiplication des entiers est: " + mult);
+        System.out.println("La division des entiers est: " + div);
+
     }
 
 }
